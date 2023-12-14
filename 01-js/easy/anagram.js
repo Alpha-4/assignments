4f1fde5d52dff0128d957bec17f643525c31dd35
Alpha-4 <aarondadhwal@gmail.com>
2023-12-14 22:45:14 +0530

    1st solution

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,11 +1,41 @@
 /*
-  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  Write a function `isAnagram` which takes 2 parameters and 
+  returns true/false if those are anagrams or not.
   What's Anagram?
-  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+  - A word, phrase, or name formed by rearranging the letters 
+  of another, such as spar, formed from rasp.
 */
 
 function isAnagram(str1, str2) {
+  let res = [],
+    res2 = [];
 
+  if (
+    (str1 === undefined && str2 !== undefined) ||
+    (str1 !== undefined && str2 === undefined) ||
+    str1.length != str2.length
+  ) {
+    return false;
+  }
+
+  for (let i = 0; i < str1.length; i++) {
+    if (res[str1[i].toLowerCase()] === undefined) {
+      res[str1[i].toLowerCase()] = 1;
+    } else {
+      res[str1[i].toLowerCase()]++;
+    }
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    if (res2[str2[i].toLowerCase()] === undefined)
+      res2[str2[i].toLowerCase()] = 1;
+    else res2[str2[i].toLowerCase()]++;
+  }
+
+  for (const [key, value] of Object.entries(res)) {
+    if (res[key] !== res2[key]) return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
