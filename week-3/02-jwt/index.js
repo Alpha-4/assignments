7d223d1ea0267ab374a1be9ea3aca36d3d0721b9
Alpha-4 <aarondadhwal@gmail.com>
2023-12-19 17:35:39 +0530

    middleware-complete

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,21 +1,28 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const jwtPassword = "secret";
 
 function signJwt(username, password) {
-
+  return jwt.sign(
+    {
+      user: username,
+      password: password,
+    },
+    jwtPassword
+  );
 }
 
 function verifyJwt(token) {
-
+  jwt.verify(token, jwtPassword);
 }
 
 function decodeJwt(token) {
-
+  const tk = verifyJwt(token);
+  return tk.user;
 }
 
 module.exports = {
-    signJwt,
-    verifyJwt,
-    decodeJwt,
-    jwtPassword
-}
+  signJwt,
+  verifyJwt,
+  decodeJwt,
+  jwtPassword,
+};
