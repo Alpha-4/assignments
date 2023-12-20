301559e3ed1da70baca9b900b080485e15742c82
Alpha-4 <aarondadhwal@gmail.com>
2023-12-20 16:28:21 +0530

    rest changes

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,6 +1,6 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
 const app = express();
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
@@ -10,18 +10,44 @@ const app = express();
 // User will be sending in their user id in the header as 'user-id'
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
+let limit = 5;
+let numberOfRequestsForUser = new Map();
 
-let numberOfRequestsForUser = {};
-setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
+const limitCheck = (req, res, next) => {
+  try {
+    const userId = req.get("user-id");
+    let reqCount = numberOfRequestsForUser.has(userId)
+      ? numberOfRequestsForUser.get(userId)
+      : 0;
+    reqCount++;
+    if (reqCount >= limit) {
+      res.status(404).json({ msg: "Limit exceeded" });
+      return res;
+    } else {
+      numberOfRequestsForUser.set(userId, reqCount);
+      next();
+    }
+  } catch (e) {
+    res.status(404).json({ msg: "Invalid Request: no userid found" });
+  }
+};
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+app.use(limitCheck);
+
+const att = setInterval(() => {
+  numberOfRequestsForUser.clear();
+}, 1000);
+
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
+
+// app.listen(3000, () => {
+//   console.log("app running on port 3000....");
+// });
 
 module.exports = app;
